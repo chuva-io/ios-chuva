0f1662c2500791be102bd8e47a355bcfc65bc7a5
Nilson Nascimento <nnascimento@redzonemap.com>
2017-11-05 18:04:56 -0500

    Animate titleLabel

--- Chuva/View Models/LargeInlineTextFieldViewModel.swift
+++ Chuva/View Models/LargeInlineTextFieldViewModel.swift
@@ -1,24 +1,3 @@
-/*
- 
- Signal
-     - Owner of Signal has unilateral control of event stream.
-     - Observers may register at any time.
-     - Observation has no side effects.
-     - Used to represent event streams that are already “in progress” (user input).
- 
- Signal Producer
-     - Defers work until SignalProducer is started.
-     - Every invocation to `start` creates a new Signal and the deferred work is started.
- 
- Property
-     - A variable that can be observed for changes.
-     - The latest value is always available.
-     - The stream will never fail.
-     - Similar to Signal.
- 
- */
-
-
 import Foundation
 import ReactiveSwift
 import Result
@@ -37,16 +16,18 @@ public protocol LargeInlineTextFieldViewModelOutputs {
     var valueText:  Property<String?> { get }
     var errorText:  Property<String?> { get }
     
-    var titleIsHidden: Property<Bool> { get }
+    var valueIsHidden: Property<Bool> { get }
 }
 
 public final class LargeInlineTextFieldViewModel: LargeInlineTextFieldViewModelType, LargeInlineTextFieldViewModelInputs,  LargeInlineTextFieldViewModelOutputs {
     
-    public init() {
-        valueText = .init(initial: "valueText",
+    public init(title: String) {
+        titleText = Property(value: title)
+        
+        valueText = .init(initial: nil,
                           then: valueTextChanged.signal)
         
-        titleIsHidden = .init(initial: false,
+        valueIsHidden = .init(initial: false,
                               then: valueText.producer.map { $0?.isEmpty ?? true })
     }
 
@@ -61,10 +42,10 @@ public final class LargeInlineTextFieldViewModel: LargeInlineTextFieldViewModelT
 
     
     //MARK: Outputs
-    public let titleText: Property<String> = Property(value: "titleText")
+    public let titleText: Property<String>
     public let valueText: Property<String?>
-    public let errorText: Property<String?> = Property(value: "errorText")
+    public let errorText: Property<String?> = Property(value: nil)
     
-    public var titleIsHidden: Property<Bool>
+    public var valueIsHidden: Property<Bool>
 
 }
--- Chuva/Views/LargeInlineTextField.swift
+++ Chuva/Views/LargeInlineTextField.swift
@@ -4,13 +4,17 @@ import ReactiveSwift
 
 @IBDesignable
 class LargeInlineTextField: UIView {
-    private let viewModel: LargeInlineTextFieldViewModelType = LargeInlineTextFieldViewModel()
+    
+    private let viewModel: LargeInlineTextFieldViewModelType = LargeInlineTextFieldViewModel(title: "first name")
     
     @IBOutlet fileprivate weak var titleLabel: UILabel!
-    @IBOutlet fileprivate weak var textField: UITextField!
+    @IBOutlet fileprivate weak var valueTextField: UITextField!
     @IBOutlet fileprivate weak var baselineView: UIView!
     @IBOutlet fileprivate weak var errorLabel: UILabel!
-
+    
+    @IBOutlet fileprivate weak var titleLabelTopAlignment: NSLayoutConstraint!
+    @IBOutlet fileprivate weak var titleLabelBottomAlignment: NSLayoutConstraint!
+    
     
     //MARK:- Reactive
     
@@ -18,16 +22,44 @@ class LargeInlineTextField: UIView {
         
         // Inputs
         titleLabel.reactive.text <~ viewModel.outputs.titleText
-        textField.reactive.text <~ viewModel.outputs.valueText
+        valueTextField.reactive.text <~ viewModel.outputs.valueText
         errorLabel.reactive.text <~ viewModel.outputs.errorText
         
-        titleLabel.reactive.isHidden <~ viewModel.outputs.titleIsHidden
+        viewModel.outputs.valueIsHidden.skipRepeats().producer.startWithSignal { observer, disposable in
+            observer.observeValues {
+                self.animateTitleLabel(position: $0 ? .bottom : .top)
+            }
+        }
         
         
         // Outputs
-        viewModel.inputs.valueTextChanged <~ textField.reactive.continuousTextValues
+        viewModel.inputs.valueTextChanged <~ valueTextField.reactive.continuousTextValues
         
     }
+    
+    //MARK:- Animations
+    
+    fileprivate enum TitleLabelPosition {
+        case top, bottom
+    }
+    
+    fileprivate func animateTitleLabel(position: TitleLabelPosition) {
+        self.layoutIfNeeded()
+        
+        switch position {
+        case .top:
+            titleLabelTopAlignment.priority = .defaultHigh
+            titleLabelBottomAlignment.priority = .defaultLow
+        case .bottom:
+            titleLabelTopAlignment.priority = .defaultLow
+            titleLabelBottomAlignment.priority = .defaultHigh
+        }
+        
+        UIView.animate(withDuration: 0.2) {
+            self.layoutIfNeeded()
+        }
+    }
+    
     
     
     //MARK:- Nib Boilerplate
